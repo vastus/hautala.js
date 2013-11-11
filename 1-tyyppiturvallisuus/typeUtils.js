29c82bae13e9c0b86ec00e640533d7f5a92f4eb5
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-11-11 20:50:59 +0200

    Freeze the utils object before returning it.

--- 1-tyyppiturvallisuus/typeUtils.js
+++ 1-tyyppiturvallisuus/typeUtils.js
@@ -2,25 +2,27 @@ var type = (function () {
 	var utils = {};
 
 	// General utility to check for the class/type of an object.
-	utils.is = function is(type, obj) {
+	function is(type, obj) {
 		var ts = Object.prototype.toString;
 		return (type === 'undefined' && obj === undefined)
 				|| (type === 'null'      && obj === null)
 				// Special check to disallow NaN as a Number.
 				|| (type === 'Number'    && !isNaN(parseFloat(obj))
-					                       && isFinite(obj))
+				                         && isFinite(obj))
 				// Detect separately.
 				|| (type === 'NaN'       && ts.call(obj).slice(8, -1) === 'Number'
-					                       && isNaN(obj))
+				                         && isNaN(obj))
 				// Check anything other than a number by it's class name.
 				|| (type !== 'Number'    && type === ts.call(obj).slice(8, -1));
 	};
+
+	utils.is = is;
 
 	// Generated helpers isNumber, isString, etc.
 	['Arguments', 'Array', 'Boolean', 'Date', 'Error', 'Function',
 	 'JSON', 'Math', 'Number', 'Object', 'RegExp', 'String', 'NaN'
 	].forEach(function (type) {
-		utils['is' + type] = utils.is.bind(null, type);
+		utils['is' + type] = is.bind(null, type);
 	});
 
 	// An util to check if given array or arguments object contains certain types.
@@ -40,7 +42,7 @@ var type = (function () {
 
 		for (var i = 0; i < length; i++) {
 			// Check if type is correct.
-			if (!utils.is(types[i], args[i])) {
+			if (!is(types[i], args[i])) {
 				return false;
 			}
 		}
@@ -60,6 +62,9 @@ var type = (function () {
 
 	utils.validation = validation.bind(null, false);
 	utils.validationOptional = validation.bind(null, true);
+
+	// We don't want anyone to fiddle with our methods.
+	Object.freeze(utils);
 
 	return utils;
 })();
