8f08039bed2f6eead549eb9214f32ef21aa469e5
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-11-12 20:46:18 +0200

    More tests, added support for errors in tests.

--- 1-tyyppiturvallisuus/typeUtilsTest.js
+++ 1-tyyppiturvallisuus/typeUtilsTest.js
@@ -4,6 +4,7 @@ var simpleTests = (function () {
 		['is("Number", new Number(1))', true],
 		['is("Number", NaN)', false],
 		['isNumber(1337)', true],
+		['isNumber("1337")', false],
 		['isNaN(0/0)', true],
 		['isNaN(undefined)', false],
 		['isRegExp(/lol/g)', true],
@@ -23,13 +24,32 @@ var simpleTests = (function () {
 		['checkOptional([], "Number", "Number", "String")', true],
 		['check([1,2], "Number", "Number|String")', true],
 		['check([1,"2"], "Number", "Number|String")', true],
-		['check([1,true], "Number", "Number|String")', false]
+		['check([1,true], "Number", "Number|String")', false],
+		['validation([1,2], "Number", "Number|String")', undefined],
+		['validation([1,"2"], "Number", "Number|String")', undefined],
+		['validation([1,true], "Number", "Number|String")', new Error()],
+		['validationOptional([1,2], "Number", "Number", "String")', undefined],
+		['validationOptional([1,2,3], "Number", "Number", "String")', new Error()],
+		['expect("Boolean", false)', false],
+		['expect("Number", 123)', 123],
+		['expect("undefined", function(){}())', undefined],
+		['expect("Boolean", "true")', new Error()],
+		['expect("Number", NaN)', new Error()],
+		['expect("Boolean", !!"true")', true],
+		['expect("Number", +"1337")', 1337]
 	];
 
 	// Run each case.
 	cases.forEach(function (test) {
-		var result = eval('type.' + test[0]);
-		if (result === test[1]) {
+		var fn = new Function('return type.' + test[0]), result;
+		try {
+			result = fn.call(null);
+		} catch (e) {
+			result = e;
+		}
+
+		if (result === test[1] ||
+			(result instanceof Error && test[1] instanceof Error)) {
 			console.log('%c' + test[0], 'color: green', '>', result);
 		} else {
 			console.warn('%c' + test[0], 'color: red', '> expected', test[1],
@@ -37,5 +57,3 @@ var simpleTests = (function () {
 		}
 	});
 });
-
-simpleTests();
