4cdae87ff5dd9c51ca8ccf161af7ab17bdc2495f
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-11-20 11:42:05 +0200

    Added functional examples

--- 2-typesAndConventions/functionalExamples.js
+++ 2-typesAndConventions/functionalExamples.js
@@ -5,8 +5,21 @@ exports.sum = function (a) {
 		s += a[i];
 	}
 	return s;
-}
+};
 
 exports.fsum = function (a) {
 	return a.reduce(function (s, v) { return s + v; }, 0);
-}
+};
+
+exports.ffact = function ffact(n) {
+	if (n === 0) { return 1; }
+	else { return n * ffact(n - 1); }	
+};
+
+exports.fact = function (n) {
+	var rval = 1;
+	for (var i = 2; i <= n; i++) {
+		rval = rval * i;
+	}
+	return rval;
+};
--- test/functional/functionalExamplesTest.js
+++ test/functional/functionalExamplesTest.js
@@ -13,3 +13,16 @@ describe('sum' , function () {
 		assert.strictEqual(examples.fsum([-1, -2, 4]), 1);
 	});
 });
+
+describe('factorial' , function () {
+	it('should work non-functional', function () {
+		assert.strictEqual(examples.fact(5), 120);
+		assert.strictEqual(examples.fact(0), 1);
+		assert.strictEqual(examples.fact(1), 1);
+	});
+	it('should work functional', function () {
+		assert.strictEqual(examples.ffact(5), 120);
+		assert.strictEqual(examples.ffact(0), 1);
+		assert.strictEqual(examples.ffact(1), 1);
+	});
+});
