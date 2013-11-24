401780b102b7c3d12979b0c5b1968e4c509ea0ea
Lalli Nuorteva <lalnuor@gmail.com>
2013-11-24 13:03:01 +0200

    glass.js

---
+++ 3-oliot ja periytyminen/glass.js
@@ -0,0 +1,39 @@
+module.exports = (function(){
+oho 
+	 function inherits(ctor, superCtor) {
+	  ctor.super_ = superCtor;
+	  ctor.prototype = Object.create(superCtor.prototype, {
+		constructor: {
+		  value: ctor,
+		  enumerable: false,
+		  writable: true,
+		  configurable: true
+		}
+	  });
+	};
+
+	function extend(origin, add) {
+	  // Don't do anything if add isn't an object
+	  if (!add || !isObject(add)) return origin;
+
+	  var keys = Object.keys(add);
+	  var i = keys.length;
+	  while (i--) {
+		origin[keys[i]] = add[keys[i]];
+	  }
+	  return origin;
+	};
+	
+	// olisi function implements mutta implements on reserved word
+	function performs(o, interf){
+		for(var kentta in interf){
+			if(typeof(kentta) == "function"){
+				if(o.kentta == interf.kentta){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+}
