88740b7575fd70b98bbaa20d972d160718f937a7
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-11-24 19:33:24 +0200

    Kyborgiesimerkki.

--- 3-oliotJaPeriytyminen/robots.js
+++ 3-oliotJaPeriytyminen/robots.js
@@ -74,3 +74,43 @@ console.log('TuhisBot osaa CookBot ja Bot:', glass.performs(TuhisBot, CookBot),
 	glass.performs(TuhisBot, Bot));
 console.log('Mutta CookBot ei osaa TuhisBot:', !glass.performs(CookBot, TuhisBot));
 console.log('Eikä Bot osaa CookBot:', !glass.performs(Bot, CookBot));
+
+function Henkilo(name) {
+	this.nimi = name;
+}
+
+Henkilo.prototype.elele = function () {
+	console.log(this.nimi + ': Oispa kaliaa.');
+};
+
+function Kyborgi(name) {
+	Henkilo.call(this, name);
+	this.virta = 100;
+}
+// Kyborgi saa henkilön ominaisuudet ja sen jälkeen botin ominaisuudet.
+glass.extend(Kyborgi.prototype, Henkilo.prototype, Bot.prototype);
+Kyborgi.prototype.constructor = Kyborgi;
+
+// Syrjätetään normaali elele-metodi.
+Kyborgi.prototype.elele = function () {
+	this.virta -= 20;
+	if (this.virta >= 0) {
+		Henkilo.prototype.elele.call(this);
+	} else {
+		console.log(this.nimi + ': Kuolin.');
+	}
+};
+
+var hauta = new Henkilo('Juho');
+var pro = new Kyborgi('Lalli');
+pro.puhu();
+for (var i = 0; i < 6; i++) {
+	hauta.elele();
+	pro.elele();
+}
+console.log('pro on Henkilo ja Bot:', glass.performs(pro, Henkilo),
+	glass.performs(pro, Bot));
+console.log('Kyborgi on Henkilo ja Bot:', glass.performs(Kyborgi, Henkilo),
+	glass.performs(Kyborgi, Bot));
+console.log('hauta on Henkilo, mutta ei Bot:', glass.performs(hauta, Henkilo),
+	!glass.performs(hauta, Bot));
