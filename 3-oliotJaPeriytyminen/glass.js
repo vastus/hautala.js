decce64b9dbfbc492ceade7ba31be214a40acc91
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-11-24 14:18:34 +0200

    Fix jsHint in glass.js

--- 3-oliotJaPeriytyminen/glass.js
+++ 3-oliotJaPeriytyminen/glass.js
@@ -1,45 +1,48 @@
+'use strict';
+
 var type = require('../lib/index').type;
+
 module.exports = (function () {
-    var utils = {};
+	var utils = {};
 
-    function inherits(ctor, superCtor) {
-        ctor.super_ = superCtor;
-        ctor.prototype = Object.create(superCtor.prototype, {
-            constructor: {
-                value: ctor,
-                enumerable: false,
-                writable: true,
-                configurable: true
-            }
-        });
-    };
+	function inherits(ctor, superCtor) {
+		ctor.super_ = superCtor;
+		ctor.prototype = Object.create(superCtor.prototype, {
+			constructor: {
+				value: ctor,
+				enumerable: false,
+				writable: true,
+				configurable: true
+			}
+		});
+	}
 
-    function extend(origin, add) {
-        // Don't do anything if add isn't an object
-        if (!add || !type.isObject(add)) return origin;
-        var keys = Object.keys(add);
-        var i = keys.length;
-        while (i--) {
-            origin[keys[i]] = add[keys[i]];
-        }
-        return origin;
-    };
+	function extend(origin, add) {
+		// Don't do anything if add isn't an object
+		if (!add || !type.isObject(add)) { return origin; }
+		var keys = Object.keys(add);
+		var i = keys.length;
+		while (i--) {
+			origin[keys[i]] = add[keys[i]];
+		}
+		return origin;
+	}
 
-    // olisi function implements mutta implements on reserved word
-    function performs(o, interf) {
-        for (var kentta in interf) {
-            if (type.isFunction(kentta)) {
-                if (o.kentta == interf.kentta) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	// olisi function implements mutta implements on reserved word
+	function performs(o, interf) {
+		for (var kentta in interf) {
+			if (type.isFunction(kentta)) {
+				if (o.kentta === interf.kentta) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
-    utils.inherits = inherits;
-    utils.extend = extend;
-    utils.performs = performs;
+	utils.inherits = inherits;
+	utils.extend = extend;
+	utils.performs = performs;
 
-    return utils;
-})
+	return utils;
+})();
